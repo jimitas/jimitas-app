78866f3e445d369d873f6f2b9e00622e1dd70749
jimitas <nonajun805@gmail.com>
2023-08-20 11:52:41 +0900

    putSelectで置き換え

--- src/pages/109kenban.tsx
+++ src/pages/109kenban.tsx
@@ -1,4 +1,5 @@
 import Layout from "@/components/Layout";
+import { PutSelect } from "@/components/PutSelect";
 import { useCallback, useEffect, useState, useRef } from "react";
 import { Kenban } from "src/components/Kenban/Kenban";
 
@@ -7,8 +8,9 @@ const ITEM = ["けんばんハーモニカ", "リコーダー", "もっきん", 
 export default function Home() {
   const el_select = useRef<HTMLSelectElement>(null);
   const [gakkiName, setGakkiName] = useState<string>("けんばんハーモニカ");
-  const changeGakki = () => {
-    setGakkiName((gakkiName) => el_select.current!.value);
+
+  const changeGakki = (e:any) => {
+    setGakkiName(e.target.value)
   };
 
   return (
@@ -16,6 +18,8 @@ export default function Home() {
       <p className="text-xs">
         スマートフォン等で上手く表示されない場合、ブラウザのメニューから「PC版で開く」を選んで表示してください。
       </p>
+      <PutSelect ITEM={ITEM} handleEvent={changeGakki} className="select m-8"></PutSelect >
+      
       <select ref={el_select} onChange={changeGakki} className="select m-8">
         {ITEM.map((item) => {
           return (
